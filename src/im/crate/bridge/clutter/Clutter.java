1ee0894cea7887babf9209814b4a40b0ae2c60b9
Aaron Miller <apage43@ninjawhale.com>
2010-08-29 06:28:37 -0400

    Text on screen

--- src/im/crate/bridge/clutter/Clutter.java
+++ src/im/crate/bridge/clutter/Clutter.java
@@ -1,13 +1,62 @@
 package im.crate.bridge.clutter;
 
-import android.app.Activity;
-import android.os.Bundle;
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.options.EngineOptions;
+import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
+import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.entity.scene.background.ColorBackground;
+import org.anddev.andengine.entity.sprite.Sprite;
+import org.anddev.andengine.entity.text.Text;
+import org.anddev.andengine.entity.util.FPSLogger;
+import org.anddev.andengine.opengl.font.Font;
+import org.anddev.andengine.opengl.texture.Texture;
+import org.anddev.andengine.opengl.texture.TextureOptions;
+import org.anddev.andengine.opengl.texture.region.TextureRegion;
+import org.anddev.andengine.opengl.texture.region.TextureRegionFactory;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
+import org.anddev.andengine.util.HorizontalAlign;
 
-public class Clutter extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+import android.graphics.Color;
+import android.graphics.Typeface;
+
+
+public class Clutter extends BaseGameActivity {
+	private static final int CAMERA_WIDTH = 720;
+	private static final int CAMERA_HEIGHT = 480;
+
+	private Camera mCamera;
+	private Font mFont;
+	private Texture mFontTexture;
+	
+	public Engine onLoadEngine() {
+		this.mCamera = new Camera(0, 0, CAMERA_WIDTH, CAMERA_HEIGHT);
+		return new Engine(new EngineOptions(true, ScreenOrientation.LANDSCAPE, new RatioResolutionPolicy(CAMERA_WIDTH, CAMERA_HEIGHT), this.mCamera));
+	}
+
+	public void onLoadResources() {
+		this.mFontTexture = new Texture(256, 256, TextureOptions.BILINEAR);
+		this.mFont = new Font(this.mFontTexture, Typeface.create(Typeface.DEFAULT, Typeface.BOLD), 32, true, Color.BLACK);
+		this.mEngine.getTextureManager().loadTexture(this.mFontTexture);
+		this.mEngine.getFontManager().loadFont(this.mFont);
+	}
+
+	public Scene onLoadScene() {
+		this.mEngine.registerUpdateHandler(new FPSLogger());
+
+		final Scene scene = new Scene(1);
+		scene.setBackground(new ColorBackground(0.09804f, 0.6274f, 0.8784f));
+
+		final Text textCenter = new Text(100, 60, this.mFont, "Zergbridge R&D\nIt's the final fucking-fuck!", HorizontalAlign.CENTER);
+		scene.getTopLayer().addEntity(textCenter);
+		
+		
+		return scene;
+	}
+
+	public void onLoadComplete() {
+		// TODO Auto-generated method stub
+		
+	}
 }
