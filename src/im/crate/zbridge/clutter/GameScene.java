58e065e7e85165f8e31264de4da07d4d8fd201ef
Aaron Miller <apage43@ninjawhale.com>
2010-08-29 03:37:05 -0400

    Man. This is silly. Committing for purpose of sharing.

--- src/im/crate/zbridge/clutter/GameScene.java
+++ src/im/crate/zbridge/clutter/GameScene.java
@@ -1,16 +1,37 @@
 package im.crate.zbridge.clutter;
 
+import java.util.ArrayList;
+
+import android.graphics.Typeface;
+
+import com.stickycoding.rokon.Font;
 import com.stickycoding.rokon.Scene;
 import com.stickycoding.rokon.background.FixedBackground;
+
 
 public class GameScene extends Scene {
 
     private FixedBackground background;
-
-    public GameScene() {
+    private ArrayList<Word> words = new ArrayList<Word>();
+    private Font uifont;
+    
+    private void addWordPair(String w1, String w2)
+    {
+    	Word first = new Word(w1, uifont);
+    	first.match = new Word(w2, uifont);
+    	words.add(first);
+    	words.add(first.match);
+    	add(first.sprite);
+    	add(first.match.sprite);
+    }
+    
+    public GameScene(Typeface mType) {
         super();
-
-        setBackground(background = new FixedBackground(Textures.background));
+        uifont = new Font(mType);
+        background = new FixedBackground(Textures.background);
+        addWordPair("test", "foo");
+        
+        setBackground(background);
     }
 
     @Override
