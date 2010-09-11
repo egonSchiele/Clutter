28363708d097ef1abc5fb37bc2a9b6227352dad4
muralig <muralig@MacBookPro17.local>
2010-09-11 04:04:04 -0700

    Re-adding the original README file.  afaik if you're not on this list, you're not part of zbridge

--- src/im/crate/bridge/clutter/Intro.java
+++ src/im/crate/bridge/clutter/Intro.java
@@ -11,53 +11,59 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.Button;
 
-public class Intro extends Activity implements OnClickListener {
-	/** Called when the activity is first created. */
-	Button start, instructions;
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-	  super.onCreate(savedInstanceState);
-      requestWindowFeature(Window.FEATURE_NO_TITLE);
-	  setContentView(R.layout.intro);
-
-	  start = (Button)findViewById(R.id.start);
-	  start.setOnClickListener(this);
-	  
-	  instructions = (Button)findViewById(R.id.instructions);
-	  instructions.setOnClickListener(this);
-	 
-	}
-	
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		if (v.equals(start)){
-			Intent intent = new Intent();
-			intent.setClass(this, Clutter.class);
-			startActivity(intent);
-		}else if (v.equals(instructions)){
-			Intent intent = new Intent();
-			intent.setClass(this, Instructions.class);
-			startActivity(intent);
-		}
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-	    super.onCreateOptionsMenu(menu);
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.settings, menu);
-	    return true;
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item)
-	{
-	    if(item.getItemId() == R.id.settings)
-	    {
-	        startActivity(new Intent(this, Prefs.class));
-	        return true;
-	    }
-	    return false;
-	}
+public class Intro extends Activity implements OnClickListener
+{
+  /** Called when the activity is first created. */
+  Button start, instructions;
+  
+  @Override
+  public void onCreate(Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    setContentView(R.layout.intro);
+    
+    start = (Button) findViewById(R.id.start);
+    start.setOnClickListener(this);
+    
+    instructions = (Button) findViewById(R.id.instructions);
+    instructions.setOnClickListener(this);
+    
+  }
+  
+  public void onClick(View v)
+  {
+    // TODO Auto-generated method stub
+    if (v.equals(start))
+    {
+      Intent intent = new Intent();
+      intent.setClass(this, Clutter.class);
+      startActivity(intent);
+    } else if (v.equals(instructions))
+    {
+      Intent intent = new Intent();
+      intent.setClass(this, Instructions.class);
+      startActivity(intent);
+    }
+  }
+  
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu)
+  {
+    super.onCreateOptionsMenu(menu);
+    MenuInflater inflater = getMenuInflater();
+    inflater.inflate(R.menu.settings, menu);
+    return true;
+  }
+  
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item)
+  {
+    if (item.getItemId() == R.id.settings)
+    {
+      startActivity(new Intent(this, Prefs.class));
+      return true;
+    }
+    return false;
+  }
 }
